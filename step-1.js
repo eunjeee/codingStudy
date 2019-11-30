9a0404585f24c3e32969cd3f7186a1d3cba488a3
eunjee <kwonji0116@daum.net>
2019-11-30 19:05:35 +0900

    step-1
    1단계: 간단 야구 게임 구현하기
    1. 게임 시작에 대한 알림 마지막에 수정 필요(alert으로 바꿀 시에)
    2. S, B, O, 안타 함수 출력 값
    (수정 해야할 사항: 각각의 배열이 호출된 횟수 값을 가져와야함)

--- step-1.js
+++ step-1.js
@@ -1,11 +1,11 @@
 var result = console.log("게임을 시작할까요?"); // confirm("게임을 시작할까요?");/* eslint no-restricted-globals:0 */
-result = true; // alert창을 띄울 시, 삭제될 영역
+result = true; // 삭제될 영역
 if (result) {
-  console.log("게임을 시작합니다!"); // alert("게임을 시작합니다!")
-  console.log("첫 번째 타자가 타석에 입장했습니다."); // alert("첫 번째 타자가 타석에 입장했습니다.")
+  console.log("게임을 시작합니다!"); // alert("")
+  console.log("첫 번째 타자가 타석에 입장했습니다."); // alert("")
 } else {
   console.log("준비가 되면 시작하겠습니다.");
-  // alert("준비가 되면 시작하겠습니다.");
+  // alert("저런~ 2018년에는 꼭 모두 이루세요!!");
 }
 
 // var list = ["스트라이크", "볼", "안타", "아웃"];
@@ -17,7 +17,7 @@ if (result) {
 
 // function makeid(){
 //     var text = "";
-//     var possible = ["스트라이크","볼","안타","아웃"];
+//     var possible = ["a","b","c","d"];
 
 //     for( var i=0; i < 2; i++ )
 //         text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -27,14 +27,45 @@ if (result) {
 // makeid();
 
 function baseBall() {
-  var ALPHA = ["스트라이크 ", "볼 ", "안타 ", "아웃 "];
+  var game = ["스트라이크", "볼", "안타", "아웃"];
   var rN = "";
-  for (var i = 0; i <= 3; i++) {
-    var randTnum = Math.floor(Math.random() * ALPHA.length);
-    rN += ALPHA[randTnum];
+  var gameCount = 0;
+
+  for (var i = 0; i <= 0; i++) {
+    var randTnum = Math.floor(Math.random() * game.length);
+    rN += game[randTnum];
+    gameCount = 0;
+    // gameCount = game[1].length;
+    // gameCount = game[2].length;
+    // gameCount = game[3].length+1;
+    gameCount++;
   }
-  console.log(rN);
+  console.log(rN + "!");
+  game[0] = "S";
+  game[1] = "B";
+  game[2] = "안타";
+  game[3] = "O";
+  console.log(gameCount);
+  console.log(
+    game[0].length,
+    game[0],
+    game[1].length,
+    game[1],
+    game[3].length,
+    game[3]
+  );
+  console.log(game[2] + "는 ", +game[2].index, "번 나왔습니다.");
+  console.log(game[0].name);
   return rN;
 }
 
-baseBall();
+// for문이나 와일문으로 돌린다.
+for (var i = 0; i < 20; i++) {
+  if (true) {
+    baseBall();
+    // 아웃 3번이면 아웃 === fasle;
+    // 게임종료 메세지
+  } else if (false) {
+    console.log("122");
+  }
+}
